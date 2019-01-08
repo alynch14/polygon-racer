ec902a0accd9758c0b1453a5ff9b033f00ac6912
Adam Lynch <alynch14@students.towson.edu>
2019-01-08 18:06:41 -0500

    Initial Commit of project.

---
+++ Polygon Racers/Assets/Scripts/Racers/Racer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racer
+{
+    private Color myColor;
+    public Racer()
+    {
+        
+    }
+}
